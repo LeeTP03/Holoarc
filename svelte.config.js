449df77405190a9fa3ae9ed9846fc46040d5111b
LeeTP03 <lyfesucksasalways@gmail.com>
2023-07-08 15:38:53 +0800

    config change

--- svelte.config.js
+++ svelte.config.js
@@ -4,15 +4,15 @@ const dev = process.argv.includes('dev');
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	preprocess: vitePreprocess(),
+	// preprocess: vitePreprocess(),
 	kit: {
 		// adapter-auto only supports some environments, see https://kit.svelte.dev/docs/adapter-auto for a list.
 		// If your environment is not supported or you settled on a specific environment, switch out the adapter.
 		// See https://kit.svelte.dev/docs/adapters for more information about adapters.
 		adapter: adapter(),
-	   	paths: {
-		base: process.env.NODE_ENV === "production" ? "/svelte-kit-site" : "",
-		},
+	   	// paths: {
+		// base: process.env.NODE_ENV === "production" ? "/svelte-kit-site" : "",
+		// },
 	}
 };
 
